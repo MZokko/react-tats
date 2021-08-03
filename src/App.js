f61ca653c466008d9c2b5d60565c674d8a3b9f6e
alexandre <akekiwy@gmail.com>
2021-08-03 18:49:39 +1000

    imp booking: format 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Pages/index';
 import Booking from './Pages/Booking';
-import BrandClothing from './Pages/BrandClothing';
-import History from './Pages/History';
 import Artist from './Pages/Artist';
+import BrandClothing from './Pages/BrandClothing';
 import Navbar from './Components/Navbar';
-import Sidebar from './Components/Sidebar';
+import Sidebar from './Components/Sidebar'
 import './App.css';
 
 function App() {
@@ -22,7 +21,7 @@ function App() {
       <Route path='/booking' component={Booking} />
       <Route path='/artist' component={Artist} />
       <Route path='/brandClothing' component={BrandClothing} />
-      <Route path='/history' component={History} />
+      {/* <Route path='/history' component={History} /> */}
     </Switch>
   );
   //Auth user routes
