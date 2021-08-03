f1c3528334ec0e8af99723c92a6ddc5aa9c431ac
alexandre <akekiwy@gmail.com>
2021-08-03 18:44:56 +1000

    imp booking: format

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import {useState} from 'react'
+import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Pages/index';
-import Booking from './Pages/Booking'
+import Booking from './Pages/Booking';
 import BrandClothing from './Pages/BrandClothing';
 import History from './Pages/History';
 import Artist from './Pages/Artist';
-import Navbar from './Components/Navbar'
-import Sidebar from './Components/Sidebar'
+import Navbar from './Components/Navbar';
+import Sidebar from './Components/Sidebar';
 import './App.css';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [isOpen,setIsOpen]=useState(false)
-
-  const toggle=()=>{
-      setIsOpen(!isOpen)
-  }
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   let routes = (
-      <Switch>
-        <Route path='/' component={Home} exact />
-        <Route path='/booking' component={Booking} />
-        <Route path='/artist' component={Artist} />
-        <Route path='/brandClothing' component={BrandClothing} />
-        <Route path='/history' component={History} />
-      </Switch>
+    <Switch>
+      <Route path='/' component={Home} exact />
+      <Route path='/booking' component={Booking} />
+      <Route path='/artist' component={Artist} />
+      <Route path='/brandClothing' component={BrandClothing} />
+      <Route path='/history' component={History} />
+    </Switch>
   );
   //Auth user routes
   // if (this.props.isAuthenticated)
@@ -32,11 +31,11 @@ function App() {
   return (
     <>
       <div className='App'>
-      <Router>
-      <Navbar toggle={toggle}/>
-      <Sidebar isOpen={isOpen} toggle={toggle}/>
-      {routes}
-      </Router>
+        <Router>
+          <Navbar toggle={toggle} />
+          <Sidebar isOpen={isOpen} toggle={toggle} />
+          {routes}
+        </Router>
       </div>
     </>
   );
