242f7ab88b52749edcd6f0f0a193b5903a6036a0
alexandre <akekiwy@gmail.com>
2021-08-03 18:27:56 +1000

    booking imp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages';
+import Home from './pages/index';
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Booking from './pages/Booking';
